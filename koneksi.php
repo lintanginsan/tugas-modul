9b7e571c419723ab2fa51b86fa3f809dd877eafb
lintanginsan <141606706+lintanginsan@users.noreply.github.com>
2024-05-11 21:24:23 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$cihuy=mysqli_connect('localhost','root','','joki');
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+?>
+
---
+++ logout.php
@@ -0,0 +1,46 @@
+<?php 
+session_start();
+
+if(isset($_POST['logout'])) {
+    
+    echo "<script>
+            var confirmLogout = confirm('Apakah Anda yakin ingin logout?');
+            if (confirmLogout) {
+                alert('Anda telah logout.');
+                window.location = 'login_petugas.php';
+            }
+ </script>";
+
+    session_destroy();
+    header('location: login_petugas.php');
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logout</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Logout</h5>
+                        <p class="card-text">Apakah Anda yakin ingin logout?</p>
+                        <form action="" method="post">
+                            <button type="submit" name="logout" class="btn btn-primary">Logout</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+</html>
